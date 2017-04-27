ce0678efbf61bb3708af97a1b41b8fe8a184c6c1
Aleksi Torhamo <aleksi@torhamo.net>
2017-04-28 00:57:26 +0300

    Generate parser from config
    Minimal allowable variation for now

--- parse.py
+++ parse.py
@@ -1,8 +1,6 @@
 import operator
 import binascii
 
-SYNC = b'\xd3\x91'
-
 def whitening_seq():
 	v = 0x1ff
 	while True:
@@ -23,16 +21,54 @@ def _crc(degree, poly, crcval, data, data_bits):
 def crc16(data):
 	return _crc(16, 0x8005, 0xffff, int(binascii.hexlify(data), 16), 8 * len(data)) & 0xffff
 
-# Default settings + doubled sync-word (IIRC)
-def parse(data):
-	assert data[:2] == b'\xAA\xAA'
-	assert data[2:6] == 2*SYNC
-	data = bytes(map(operator.xor, data[6:], whitening_seq()))
-	assert crc16(data) == 0
-	return data[:-2]
+def make_parser(conf):
+	own_length = conf.field.PACKET_LENGTH
+	addr_mode = conf.field.ADR_CHK
+	white_data = conf.field.WHITE_DATA
+	crc_en = conf.field.CRC_EN
+	length_mode = conf.field.LENGTH_CONFIG
+	own_addr = conf.field.DEVICE_ADDR
 
-if __name__ == '__main__':
-	data = '10101010101010101101001110010001110100111001000111111011001010111110001100100000010100110011101101100100'
-	data = int(data, 2).to_bytes(len(data)//8, 'big')
-	print(parse(data))
+	assert addr_mode == 0, 'Address not supported (yet)'
+	assert length_mode == 1, 'Fixed length not supported (yet)'
+	assert not conf.field.CC2400_EN, 'CC2400 mode not supported (yet)'
+	assert length_mode in {0, 1}, 'Infinite packet length not supported'
+	assert not conf.field.MANCHESTER_EN, 'Manchester encoding not supported (yet)'
+	assert not conf.field.FEC_EN, 'FEC not supported (yet)'
+
+	preamble = b'\xAA'
+	preamble *= [2, 3, 4, 6, 8, 12, 16, 24][conf.field.NUM_PREAMBLE]
+
+	sync_word = bytes([conf.reg.SYNC1, conf.reg.SYNC0])
+	sync_count = [0, 1, 1, 2][conf.field.SYNC_MODE % 4]
+
+	sync_word *= sync_count
+	if not sync_count:
+		preamble *= 0
+
+
+	def parse(data):
+		assert data[:len(preamble)] == preamble
+		data = data[len(preamble):]
+		assert data[:len(sync_word)] == sync_word
+		data = data[len(sync_word):]
+
+		if white_data:
+			data = bytes(map(operator.xor, data, whitening_seq()))
+
+		length = data[0]
+		assert length <= own_length
+
+		addr = None
+
+		data = data[:length+1+2]
+
+		if crc_en:
+			assert crc16(data) == 0
+
+		payload = data[1:length+1]
+
+		return (addr, payload)
+
+	return parse
 
